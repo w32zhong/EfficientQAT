b875af8053f430be7ad3fce7fd8b3278a1ba26de
w32zhong <clock126@126.com>
2024-08-30 16:35:38 -0400

    test.py: added baseline ref model.

--- test.py
+++ test.py
@@ -1,8 +1,30 @@
 import time
-from transformers import AutoTokenizer
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import TextStreamer
 from gptqmodel import GPTQModel
 
+# ref model
+ref_model_path = "NousResearch/Llama-2-7b-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(ref_model_path)
+model = AutoModelForCausalLM.from_pretrained(ref_model_path,
+    torch_dtype=torch.float16, device_map='auto', load_in_8bit=False)
+streamer = TextStreamer(tokenizer)
+
+start = time.time()
+output = model.generate(
+    **tokenizer("Solar eclipse is ", return_tensors="pt").to(model.device),
+    max_new_tokens=256, streamer=streamer, use_cache=True
+)
+end = time.time()
+
+output_len = output.shape[-1]
+delta_time = end - start
+print(output_len, delta_time, output_len / delta_time)
+
+
+# 2-bit model in BitBLAS
 model_path = "ChenMnZ/Llama-2-7b-EfficientQAT-w2g64-BitBLAS"
 
 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)

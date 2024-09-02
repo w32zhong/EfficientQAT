59f9ef5227c795c0c0a48902b3a0577248661c9a
w32zhong <clock126@126.com>
2024-09-01 22:22:59 -0400

    myhook analyze

--- myhook.py
+++ myhook.py
@@ -1,7 +1,9 @@
+import fire
 import torch
 import pickle
 from functools import partial
 from collections import defaultdict
+
 
 def hook_fn(collect, model, path, module, inputs, output):
     x = inputs[0]
@@ -22,6 +24,35 @@ def add(model, limited_paths=None):
             )
     return collect
 
+
 def save(collect):
-    with open('collect.plk', 'wb') as fh:
+    with open('collect.pkl', 'wb') as fh:
         pickle.dump(collect, fh)
+
+
+def analyze(filename='collect.pkl',
+    channels=range(1170, 1180), timesteps=slice(-1), io=1, do_act=False, act_base=0.2):
+
+    with open('collect.pkl', 'rb') as fh:
+        collect = pickle.load(fh)
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    act_fn = lambda x: max(x, act_base) - act_base if do_act else x
+    for key, values in collect.items():
+        values = values[timesteps]
+        for chann in channels:
+            ys = [act_fn(time_values[io][chann]) for time_values in values]
+            xs = np.arange(len(ys))
+            ax.bar(xs, ys, zs=chann, zdir='y', alpha=0.8)
+        ax.set_title(f'{key} ({"input" if io == 0 else "output"})')
+    ax.set_xlabel('time')
+    ax.set_ylabel('channel')
+    ax.set_zlabel('activation')
+    ax.set_yticks(list(channels))
+    plt.show()
+
+if __name__ == '__main__':
+    fire.Fire(analyze)
--- test.py
+++ test.py
@@ -7,26 +7,26 @@ from gptqmodel import GPTQModel
 from quantize.int_linear_real import load_quantized_model
 
 
-# ref model
-ref_model_path = "NousResearch/Llama-2-7b-hf"
-
-tokenizer = AutoTokenizer.from_pretrained(ref_model_path)
-model = AutoModelForCausalLM.from_pretrained(ref_model_path,
-    torch_dtype=torch.bfloat16, device_map='auto', load_in_8bit=False)
-streamer = TextStreamer(tokenizer)
-
-start = time.time()
-output = model.generate(
-    **tokenizer("Solar eclipse is ", return_tensors="pt").to(model.device),
-    max_new_tokens=256, streamer=streamer, use_cache=True
-)
-end = time.time()
-
-output_len = output.shape[-1]
-delta_time = end - start
-print(output_len, delta_time, output_len / delta_time)
-del model
-torch.cuda.empty_cache()
+# # ref model
+# ref_model_path = "NousResearch/Llama-2-7b-hf"
+# 
+# tokenizer = AutoTokenizer.from_pretrained(ref_model_path)
+# model = AutoModelForCausalLM.from_pretrained(ref_model_path,
+#     torch_dtype=torch.bfloat16, device_map='auto', load_in_8bit=False)
+# streamer = TextStreamer(tokenizer)
+# 
+# start = time.time()
+# output = model.generate(
+#     **tokenizer("Solar eclipse is ", return_tensors="pt").to(model.device),
+#     max_new_tokens=256, streamer=streamer, use_cache=True
+# )
+# end = time.time()
+# 
+# output_len = output.shape[-1]
+# delta_time = end - start
+# print(output_len, delta_time, output_len / delta_time)
+# del model
+# torch.cuda.empty_cache()
 
 
 # 2-bit model in triton
@@ -34,6 +34,9 @@ local_dir = '~/.cache/huggingface/hub/models--ChenMnZ--Llama-2-7b-EfficientQAT-w
 model, tokenizer = load_quantized_model(os.path.expanduser(local_dir), 2, 64)
 streamer = TextStreamer(tokenizer)
 model.to('cuda:1')
+import myhook
+collect = myhook.add(model, ['model.layers.6.mlp.down_proj'])
+
 start = time.time()
 output = model.generate(
     **tokenizer("Solar eclipse is ", return_tensors="pt").to(model.device),
@@ -43,28 +46,29 @@ end = time.time()
 output_len = output.shape[-1]
 delta_time = end - start
 print(output_len, delta_time, output_len / delta_time)
+myhook.save(collect)
 del model
 torch.cuda.empty_cache()
 
 
-# 2-bit model in BitBLAS
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-model_path = "ChenMnZ/Llama-2-7b-EfficientQAT-w2g64-BitBLAS"
-
-tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
-model = GPTQModel.from_quantized(model_path)
-model.to('cuda')
-streamer = TextStreamer(tokenizer)
-
-start = time.time()
-output = model.generate(
-    **tokenizer("Solar eclipse is ", return_tensors="pt").to(model.device),
-    max_new_tokens=256, streamer=streamer, use_cache=True
-)
-end = time.time()
-
-output_len = output.shape[-1]
-delta_time = end - start
-print(output_len, delta_time, output_len / delta_time)
-del model
-torch.cuda.empty_cache()
+# # 2-bit model in BitBLAS
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# model_path = "ChenMnZ/Llama-2-7b-EfficientQAT-w2g64-BitBLAS"
+# 
+# tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
+# model = GPTQModel.from_quantized(model_path)
+# model.to('cuda')
+# streamer = TextStreamer(tokenizer)
+# 
+# start = time.time()
+# output = model.generate(
+#     **tokenizer("Solar eclipse is ", return_tensors="pt").to(model.device),
+#     max_new_tokens=256, streamer=streamer, use_cache=True
+# )
+# end = time.time()
+# 
+# output_len = output.shape[-1]
+# delta_time = end - start
+# print(output_len, delta_time, output_len / delta_time)
+# del model
+# torch.cuda.empty_cache()
